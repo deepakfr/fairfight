2ae89716d1aca874110df8367e102bc1e457b4e4
deepakfr2 <deepakfr99@gmail.com>
2025-04-25 13:57:21 +0200

    st correction

--- whoisright.py
+++ whoisright.py
@@ -178,7 +178,7 @@ def main():
     st.title("🤖 FairFight AI")
     st.caption("Because every conflict deserves a fair verdict.")
 
-    query = st.experimental_get_query_params()
+    query = st.query_params()
     step = query.get("step", ["1"])[0]
 
     if step == "2":
